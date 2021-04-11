20c0fb361b36b386d083635c9b1e6b90c5410a04
luisSantamariaCOL <luisestebansantamaria@outlook.com>
2021-04-11 12:30:23 -0500

    usuario admin creado | CRUD Version 1 creado | App gestionVacunas

--- gestionVacunas/models.py
+++ gestionVacunas/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Vacunado(models.Model):
+    nombre=models.CharField(max_length=80)
+    email=models.EmailField(blank=True, null=True)
